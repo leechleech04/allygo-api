66e3594996dc8d791428a85cc631a4df46eda473
lch2023 <lch2023@naver.com>
2026-07-28 00:13:49 +0900

    Init: initialize allygo-api

---
+++ src/main/java/com/allygo/allygo_api/AllygoApiApplication.java
@@ -0,0 +1,13 @@
+package com.allygo.allygo_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllygoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllygoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/allygo/allygo_api/AllygoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allygo.allygo_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllygoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
